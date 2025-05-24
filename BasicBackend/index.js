96734caeeaccaf8b197ffd4ee70372299dc106bb
Mahesh <70122194+mahesh1105@users.noreply.github.com>
2025-05-24 18:57:40 +0000

    Fetch JSON Data with URL

--- BasicBackend/index.js
+++ BasicBackend/index.js
@@ -3,6 +3,42 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const githubData = {
+  "login": "mahesh1105",
+  "id": 70122194,
+  "node_id": "MDQ6VXNlcjcwMTIyMTk0",
+  "avatar_url": "https://avatars.githubusercontent.com/u/70122194?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/mahesh1105",
+  "html_url": "https://github.com/mahesh1105",
+  "followers_url": "https://api.github.com/users/mahesh1105/followers",
+  "following_url": "https://api.github.com/users/mahesh1105/following{/other_user}",
+  "gists_url": "https://api.github.com/users/mahesh1105/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/mahesh1105/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/mahesh1105/subscriptions",
+  "organizations_url": "https://api.github.com/users/mahesh1105/orgs",
+  "repos_url": "https://api.github.com/users/mahesh1105/repos",
+  "events_url": "https://api.github.com/users/mahesh1105/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/mahesh1105/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Mahesh",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": "Passionate Learner",
+  "twitter_username": null,
+  "public_repos": 6,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2020-08-24T02:38:41Z",
+  "updated_at": "2025-05-14T07:29:18Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +53,10 @@ app.get('/login', (req, res) => {
 
 app.get('/youtube', (req, res) => {
   res.send('<h2>Welcome to YouTube</h2>')
+})
+
+app.get('/github', (req, res) => {
+  res.json(githubData)
 })
 
 // App will listen on the port for the requests
